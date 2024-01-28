10c4c60ce5d32f17b558f12395a09bf7fbc376c5
degull <skytmdgus99@gmail.com>
2024-01-28 16:24:34 +0900

    app.js 수정

---
+++ src/MainComponents/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './Main.styled';
+
+export default function Main(){
+   return (
+      <S.Wrapper>
+         <S.Container>
+            <h1>메인이요</h1>
+         </S.Container>
+      </S.Wrapper>        
+      
+   );
+};
+
