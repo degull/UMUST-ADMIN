c192b3c9eca3d4ac9aefe882f8b21c897c93fa30
degull <skytmdgus99@gmail.com>
2024-01-28 18:26:05 +0900

    메인 레이아웃 완료

--- src/MainComponents/Menu.js
+++ src/MainComponents/Menu.js
@@ -5,9 +5,27 @@ export default function Menu(){
    return (
       <S.FlexContainer>
          <S.Navigation>
-            <S.NavItem>Home</S.NavItem>
-            <S.NavItem>About</S.NavItem>
-            <S.NavItem>Contact</S.NavItem>         
+            <S.Research>연구개발</S.Research>
+               <S.Paper> - 논문</S.Paper>
+               <S.Patent> - 특허</S.Patent>
+               <S.Certification> - 인증현황</S.Certification>
+               <S.Blog> - 연구소 블로그</S.Blog>
+               
+            <S.Product>제품</S.Product>
+               <S.Info> - 관련정보</S.Info>
+
+
+            <S.CRO>CRO 서비스</S.CRO>     
+               <S.Data> - 자료실</S.Data>    
+
+            
+            <S.Board>알림마당</S.Board>    
+               <S.Notice> - 공지사항</S.Notice> 
+               <S.Press> - 보도자료</S.Press> 
+               <S.Event> - 행사정보</S.Event>
+               <S.Album> - 앨범</S.Album>
+
+
          </S.Navigation>
       </S.FlexContainer>
    );
--- src/MainComponents/Menu.styled.js
+++ src/MainComponents/Menu.styled.js
@@ -9,15 +9,148 @@ export const FlexContainer = styled.div`
 
 
 export const Navigation = styled.nav`
+   width: 200px;
+   height: 100%;
    position: absolute;
    display: flex;
    flex-direction: column;
-/*    background: #8c8;
- */   left: 0;
-   margin-top: 80px;
+   left: 0;
+   margin-top: 50px;
    margin-left: 30px;
-
+   padding-bottom: 40px;
+   border-right: 1px solid #999;
 `;
+/* 연구개발 */
+export const Research = styled.div`
+   margin-top: 25px;
+
+   font-size: 13px;
+   font-weight: bold;
+   padding :10px;
+   cursor: pointer;
+   margin-bottom: 10px;
+   border-bottom: 1px solid #ddd;
+   &:hover {
+      background: #ddd;
+   }
+`;
+
+// 논문
+export const Paper = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+// 특허
+export const Patent = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+// 인증현황
+export const Certification = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+// 연구소 블로그
+export const Blog = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+/* 연구개발 */
+export const Product = styled.div`
+   margin-top: 25px;
+   font-size: 13px;
+   font-weight: bold;
+   padding: 10px;
+   cursor: pointer;
+   border-bottom: 1px solid #ddd; /* 메뉴 간에 구분선 추가 */
+   &:hover {
+      background: #ddd;
+   }
+`;
+
+// 관련정보
+export const Info = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+
+/* CRO 서비스 */
+export const CRO = styled.div`
+  font-size: 13px;
+  margin-top: 25px;
+
+   font-weight: bold;
+   padding: 10px;
+   cursor: pointer;
+   border-bottom: 1px solid #ddd; /* 메뉴 간에 구분선 추가 */
+   &:hover {
+      background: #ddd;
+   }
+`;
+
+
+// 자료실
+export const Data = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+/* 알림마당 */
+export const Board = styled.div`
+  font-size: 13px;
+   font-weight: bold;
+   margin-top: 25px;
+   padding: 10px;
+   cursor: pointer;
+   border-bottom: 1px solid #ddd; /* 메뉴 간에 구분선 추가 */
+   &:hover {
+      background: #ddd;
+   }
+`;
+
+
+// 공지사항
+export const Notice = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+
+// 보도자료
+export const Press = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+
+// 행사정보
+export const Event = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
+
+// 앨범
+export const Album = styled.div`
+   font-size: 11px;
+   padding: 10px 0 10px 10px;
+   cursor: pointer;
+`;
+
 
 export const NavItem = styled.div`
    padding: 10px;
@@ -27,3 +160,4 @@ export const NavItem = styled.div`
       background: #ddd;
    }
 `;
+
